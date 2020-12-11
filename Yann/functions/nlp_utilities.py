c55a70aa2e210614a85661bc005dfdb71eb141c4
Yann <y.besselievre2@gmail.com>
2020-12-11 14:34:13 +0100

    Ajout LDA + NMF et tests dans notebook

--- Yann/functions/nlp_utilities.py
+++ Yann/functions/nlp_utilities.py
@@ -7,8 +7,8 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.corpus import words
 from string import punctuation
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.decomposition import NMF, LatentDirichletAllocation
 from gensim.models import Word2Vec
 
 class LemmaTokenizer(object):
@@ -60,6 +60,16 @@ class FrenchStemTokenizer(object):
         word_list = [word for word in word_list if word.isalpha()]
         return [self.st.stem(t) for t in word_list]
     
+    
+def print_topics(model, feature_names, n_words):
+    '''Print topics of given LDA or NMF model'''
+    for topic_idx, topic in enumerate(model.components_):
+        message = "Topic #%d: " % topic_idx
+        message += " ".join([feature_names[i]
+                             for i in topic.argsort()[:-n_words - 1:-1]])
+        print(message)
+
+
 def get_stop_words(words_to_add=[], words_to_delete=[], language = 'en'):
     if language == 'fr':
         stop_words = set(stopwords.words('french'))
@@ -77,19 +87,23 @@ def get_stop_words(words_to_add=[], words_to_delete=[], language = 'en'):
         else:
             print(f'Word {word_to_delete} not in original stop words list\n')
     return stop_words
-        
-def df_to_bow(df, stop_words = None, language = 'en', TFIDF = True):
-    '''Returns the BOW (model and feature mapping) of a given "ticker" dataframe. 
-    If TFIDF is true, the BOW is weighted with "Term frequency times inverse document frequency" 
-    (generally better performances)'''
-    text_df = df["Text"]
+
+def get_tokenizer(stop_words = None, language = 'en'):
     if language == 'fr':
         tokenizer = FrenchStemTokenizer(stop_words = stop_words)
     elif language == 'en':
         tokenizer = LemmaTokenizer(stop_words = stop_words)
     else :
         raise LanguageError("Language not supported")
-    countvect = CountVectorizer(tokenizer)
+    return tokenizer
+        
+def df_to_bow(df, stop_words = None, language = 'en', TFIDF = True):
+    '''Returns the BOW (model and feature mapping) of a given "ticker" dataframe. 
+    If TFIDF is true, the BOW is weighted with "Term frequency times inverse document frequency" 
+    (generally better performances)'''
+    text_df = df["Text"]
+    tokenizer = get_tokenizer(stop_words, language)
+    countvect = CountVectorizer(tokenizer = tokenizer, max_df=0.95, min_df=2)
     bow = countvect.fit_transform(text_df)
     feat2word = {v: k for k, v in countvect.vocabulary_.items()}
     if TFIDF :
@@ -99,12 +113,25 @@ def df_to_bow(df, stop_words = None, language = 'en', TFIDF = True):
 def df_to_vec(df, stop_words = None, language = 'en', size=200, window=5, min_count=1):
     '''Returns the Word2Vec model of a given "ticker" dataframe.'''
     text_df = df["Text"]
-    if language == 'fr':
-        tokenizer = FrenchStemTokenizer(stop_words = stop_words)
-    elif language == 'en':
-        tokenizer = LemmaTokenizer(stop_words = stop_words)
-    else :
-        raise LanguageError("Language not supported")
+    tokenizer = get_tokenizer(text_df, stop_words, language)
     text_for_word2vec=[tokenizer(text) for text in text_df]
     model = Word2Vec(text_for_word2vec,size=size,window=window,min_count=min_count)
     return model
+
+def df_to_lda(df, n_topics = 5, stop_words = None, language = 'en', TF = True):
+    text_df = df["Text"]
+    tokenizer = get_tokenizer(stop_words, language)
+    countvect = CountVectorizer(tokenizer = tokenizer, max_df=0.95, min_df=2)
+    bow = countvect.fit_transform(text_df)
+    feat2word = {v: k for k, v in countvect.vocabulary_.items()}
+    if (TF):
+        bow = TfidfTransformer(use_idf = False).fit_transform(bow)
+    lda = LatentDirichletAllocation(n_components=n_topics, max_iter=5, learning_method='online',
+                                    learning_offset=50., random_state=0)
+    lda.fit(bow)
+    return lda, countvect, feat2word
+
+def df_to_nmf(df, n_topics=5, stop_words = None, language = 'en'):
+    bow, countvect, feat2word = df_to_bow(df, stop_words = stop_words, language = language, TFIDF = True)
+    nmf = NMF(n_components=n_topics, alpha=.1, l1_ratio=.5).fit(bow)
+    return nmf, countvect, feat2word
