5f68f254fba00b8716d070a37203627310e25a07
zexindai <zexin.dai@gmail.com>
2021-01-17 01:19:38 +0100

    add format_cols as enter

--- Zexin/search_author.py
+++ Zexin/search_author.py
@@ -1,10 +1,9 @@
 import tweepy as tw
 import pandas as pd
-
-def search_author(search_id, items, retweet=False, reply = False):
-
+def search_author(search_id, items, codes,format_cols, retweet=False, reply = False):
     # items : le nombre de tweet max retourné
     # codes = ['API_Key', 'API_Secret_Key', 'Access_Token','Access_Secret_Token']
+    # format_cols: list like ["Text", "Author", "Date"]
     
     auth = tw.OAuthHandler(codes[0], codes[1])
     auth.set_access_token(codes[2],codes[3])
@@ -12,14 +11,13 @@ def search_author(search_id, items, retweet=False, reply = False):
     
     try:
         api.verify_credentials()
-        print("Authentification ok")
     except tw.TweepError:
         print("Error during authentification")
 
-    tweets = tw.Cursor(api.user_timeline, screen_name = search_id, count = items, include_rts = retweet,exclude_replies = not reply, tweet_mode='extended').items(items)
+    tweets = tw.Cursor(api.user_timeline, screen_name = search_id, count = items, include_rts = retweet,exclude_replies= not reply, tweet_mode='extended').items(items)
 
     list_data = [[tweet.full_text, tweet.user.screen_name, tweet.created_at] for tweet in tweets]
 
     tweet_data = pd.DataFrame(data=list_data, columns=["Text", "Author", "Date"])
 
-    return tweet_data
+    return tweet_data[format_cols]
